9ed501175d8311ed12f4e9f247db0c821a47b11d
crondonveritone <crondon@veritone.com>
2024-12-14 18:13:20 -0300

    init commit

--- admin_service/main.go
+++ admin_service/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	// Crear una nueva instancia de Fiber
+	app := fiber.New()
+
+	// Ruta raíz con un handler que responde "Hola Mundo"
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(fmt.Sprintf("hello world v2..., %s", os.Getenv("JWT_KEY")))
+	})
+
+	// Escuchar en el puerto 4000
+	log.Println("Servidor corriendo en http://localhost:4000")
+	log.Fatal(app.Listen(":4000"))
 }
